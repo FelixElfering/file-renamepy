bc06dfea625de3357a6b99d11621f2aec07a17d5
Felix Elfering <32690312+FelixElfering@users.noreply.github.com>
2021-03-24 12:52:40 +0100

    add gui and main functionality

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # main.py
 from pathlib import Path
 import os
+import PySimpleGUI as sg
+
+fileList: list[str]
+filenameList: list[str]
 
 
 # renames all files using the list of filenames, while retaining the file extension
@@ -10,7 +14,39 @@ def rename_files(filenames: list[str], files: list[str]) -> None:
 
     for i, file in enumerate(files):
         path = Path(file)
-        new_path = os.path.join(path.parent, filenames[i], path.suffix)
+        new_path = os.path.join(path.parent, filenames[i] + path.suffix)
         os.rename(file, new_path)
 
 
+# parse filename list from string
+def parse_filenames(filename_string: str) -> list[str]:
+    return filename_string.replace("\n", "").split(",")
+
+
+# gui with main functionality
+def gui() -> None:
+    sg.theme("Dark Blue 3")
+    layout = [
+        [sg.Text("Welcome to file-renamepy")],
+        [sg.Text("Choose files to be renamed:"), sg.FilesBrowse(key="-SELECTED_FILES-", target=(None, None))],
+        [sg.Text("Enter new filenames:"), sg.Multiline(key="-FILENAMES-")],
+        [sg.Button("Rename Files")]
+    ]
+    window = sg.Window("file-renamepy", layout)
+    event, values = window.read()
+
+    # Extract values
+    global fileList, filenameList
+    fileList = values["-SELECTED_FILES-"].split(";")
+    print("Selected files: ", fileList)
+    filenameList = parse_filenames(values["-FILENAMES-"])
+    print("Filenames: ", filenameList)
+
+    # Action
+    rename_files(filenameList, fileList)
+
+    window.close()
+
+
+if __name__ == "__main__":
+    gui()
